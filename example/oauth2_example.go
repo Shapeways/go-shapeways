ed4c7c10a21a642f607030c2b8c8398177e3758b
matt <matt@shapeways.com>
2018-07-26 08:11:02 -0400

    Update comments

--- example/oauth2_example.go
+++ example/oauth2_example.go
@@ -6,8 +6,7 @@ import "fmt"
 import "../shapeways"
 
 func main() {
-  client := shapeways.NewClient("ENPbso1i8tyUWX2tmMhHK5pm8pcHN9cauV96CVALkaIQUa3qQr", "j7aEPlq83n9UtU0t3u1dg6Gk1Bw1CwU4TsTosKTjTSwl3GVQ0y")
-  // client := shapeways.NewClient("iOUMAH66yc67yPOIUWn9vLFTtFeVyjAIXclUbq2vNCcYf1Cbnf", "O7O5VPXeIQStWjX6OmeGlrVnCkpzUbuAY4jnoYLZGPjYQaLQr0")
+  client := shapeways.NewClient("YOUR CLIENT KEY", "YOUR CLIENT SECRET")
   status, err := client.Authenticate()
   if err != nil {
     fmt.Println(status)
